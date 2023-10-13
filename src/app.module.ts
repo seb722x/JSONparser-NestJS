e233fc52aaa12e2b67f22c251df21856abbdaeb7
sebastian rodriguez <juan.sebas-722@hotmail.com>
2023-10-13 18:05:19 -0500

    app.module modificated to integrate all modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 
+import { AppController } from './app.controller';
+import { JsonMapperModule } from './json-mapper/json-mapper.module';
+
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { MailparserModule } from './mail-parser/mailparser.module';
 
 @Module({
-  imports: [],
+  imports: [
+    JsonMapperModule,
+    MailparserModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
